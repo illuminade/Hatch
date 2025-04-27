8dc08817caae57e0b770b976362a2a8fc6a9bab7
illuminade <illuminade@gmail.com>
2025-04-27 11:21:47 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Replace with your actual Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtE69gVgf35jCKKrN76ZvyEUYAF5-GtGo",
+  authDomain: "hatch-575a7.firebaseapp.com",
+  projectId: "hatch-575a7",
+  storageBucket: "hatch-575a7.firebasestorage.app",
+  messagingSenderId: "379734165477",
+  appId: "1:379734165477:web:e0e5ed6c3df0b88eed110c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const eggsCollection = db.collection('eggs');
